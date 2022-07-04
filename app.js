1231e751b62e58344077ff9a01c99c3095dd4672
rholibobo <rholibobo@gmail.com>
2022-07-04 15:46:27 +0100

    blog page with ejs

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+
+const port = 4000;
+
+app.get('/', (req, res)=>{
+    res.render('index')
+})
+
+app.get('/about', (req, res)=>{
+    res.render('about')
+})
+
+app.get('/contact', (req, res)=>{
+    res.render('contact')
+})
+
+
+
+app.listen(port, ()=>{
+    console.log(`server is running at ${port}`);
+})
